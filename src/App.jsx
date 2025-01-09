3ecb58c576ce884d444a75807ca90874ed7fbae2
SNelson723 <stephenn649@gmail.com>
2025-01-08 18:55:55 -0600

    Setup the Routes/Route tree in App.jsx and centered the text of the rendered container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Navbar from "./Navbar";
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Pricing from './Pages/Pricing';
+import About from './Pages/About';
 
 const App = () => {
 
   return (
     <>
       <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
     </>
   )
 }
