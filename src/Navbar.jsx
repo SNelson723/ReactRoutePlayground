8e99115faad365b6cb30370601435fa1547b2d7a
SNelson723 <stephenn649@gmail.com>
2025-01-08 19:07:04 -0600

    Set upt he playground successfully and made some comments, going to go back through and take note of what is going on so I can reference the basics down the road, should I need to

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,38 @@
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">Site Name</a>
+      <Link to="/" className="site-title">Site Name</Link>
       <ul>
-        <li>
-          <a href="/pricing" className="">Pricing</a>
-        </li>
-        <li>
-          <a href="/about" className="">About</a>
-        </li>
+        <CustomLink to='/pricing'>Pricing</CustomLink>
+        <CustomLink to='/about'>About</CustomLink>
       </ul>
     </nav>
   )
+};
+
+const CustomLink = ({ to, children, ...props }) => {
+  /**
+   * useResolvedPath allows you to take a relative/absolute path
+   * and combines it with the current path you're on and gives
+   * you the actual full path you're accessing
+   *
+   */
+  const resolvedPath = useResolvedPath(to);
+
+  /**
+   * useMatch just matches an input path with the path you're currently
+   * on and determines what the window's location is
+   */
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+  return (
+    <li className={isActive ? 'active': ''}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 };
 
 export default Navbar;
